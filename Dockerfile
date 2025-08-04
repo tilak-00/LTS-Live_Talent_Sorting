4a106fe428b4b3d7f469311f15d84693df0a8a92
tilak-00 <tilakkumar7989@gmail.com>
2025-08-04 18:40:33 +0530

    Modify requirementtss

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-COPY /etc/secrets/.env .env  
+RUN test -f /etc/secrets/.env && cp /etc/secrets/.env .env || echo ".env not found at build time"
+
 # Secret file Render provides
 
 RUN pip install python-dotenv
--- resume_ranker/settings.py
+++ resume_ranker/settings.py
@@ -1,6 +1,11 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()  # Load from .env into environment
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
